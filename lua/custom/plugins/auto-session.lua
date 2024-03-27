bac1790190607323bfba4c6f12aace2271ea06f1
hugovntr <26794193+hugovntr@users.noreply.github.com>
2024-03-27 07:10:18 +0100

    fix: enable auto-session

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -3,11 +3,19 @@ return {
   config = function()
     require('auto-session').setup {
       auto_session_enabled = true,
+      auto_save_enabled = true,
+      auto_restore_enabled = true,
       auto_session_enable_last_session = false,
       auto_session_root_dir = vim.fn.stdpath 'data' .. '/sessions/',
       auto_session_last_session_dir = vim.fn.stdpath 'data' .. '/sessions/',
       log_level = 'error',
       auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      session_lens = {
+        buftypes_to_ignore = {},
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
     }
 
     -- Set mapping for Telescope
