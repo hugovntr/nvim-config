2039c5469b88a559b347ed4826bd254755521d24
hugovntr <26794193+hugovntr@users.noreply.github.com>
2024-05-01 18:27:04 +0200

    feat: add ruff-lsp

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -136,7 +136,16 @@ return { -- LSP Configuration & Plugins
     local servers = {
       -- clangd = {},
       gopls = {},
-      pyright = {},
+      pyright = {
+        disableOrganizeImports = true,
+      },
+      ruff_lsp = {
+        init_options = {
+          settings = {
+            args = {},
+          },
+        },
+      },
       -- rust_analyzer = {},
       -- -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
@@ -176,7 +185,8 @@ return { -- LSP Configuration & Plugins
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
-      'black',
+      -- 'black',
+      'ruff',
       'prettierd',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
